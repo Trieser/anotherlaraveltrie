fbf1f480eac0cc26029670ba1e04d807c181ae11
Dmitrie <dmitriewinata@gmail.com>
2024-12-16 17:08:24 +0700

    test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vue App</title>
+    <title>Vue App TEST</title>
     @vite('resources/js/app.js')
 </head>
 
