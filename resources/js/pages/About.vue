217a70c0462fd06f900f53d1833e10cb9a1e0368
Dmitrie <dmitriewinata@gmail.com>
2024-12-16 17:17:06 +0700

    Test About Dmitrie

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1 class="text-2xl font-bold mb-4">About Page</h1>
+        <h1 class="text-2xl font-bold mb-4">About Page Dmitrie</h1>
         <p>This is the About page.</p>
     </div>
 </template>
